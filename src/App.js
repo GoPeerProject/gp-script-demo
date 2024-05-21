41286c12892b05625412879f019daa62a3e179ff
Erik Laucks <eriklaucks@gmail.com>
2024-05-20 21:51:02 -0400

    Update demo

--- src/App.js
+++ src/App.js
@@ -1,50 +1,114 @@
-import logo from "./logo.svg";
-import "./App.css";
-import { useEffect } from "react";
+import logo from './logo.svg'
+import './App.css'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
-let theme = "dark";
+let theme = 'dark'
 
 function App() {
+  const [org, setOrg] = useState('')
+  const [key, setKey] = useState('')
+  const [token, setToken] = useState('')
+  const [launched, setLaunched] = useState(false)
+
   useEffect(() => {
-    setTimeout(() => {
-      const gP = window.gP;
-      // setup env
-      gP.setEnv("dev");
-      const token = "<token>"; //token loaded from server after making request to gopeer-api
-      gP.setToken(token);
+    const gpPmid = localStorage.getItem('gp-pmid') ?? ''
+    const gpKey = localStorage.getItem('gp-key') ?? ''
+    if (gpPmid !== org) setOrg(gpPmid)
+    if (gpKey !== key) setKey(gpKey)
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-      // auth user
+  const loadToken = async () => {
+    try {
+      const res = await axios.post(
+        'https://dev.gopeer.org/organizations/identify',
+        { orgId: org, key }
+      )
+      setToken(res.data.token)
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
-      gP.identify({
-        userId: "<user-id>",
-        firstName: "John",
-        lastName: "Doe",
-      });
+  const showButton = () => {
+    const gP = window.gP
+    // setup env
+    gP.setEnv('dev')
+    gP.setToken(token)
 
-      // show and position widget
-      gP.show();
-      gP.setButtonStyles({ bottom: "50px", right: "calc(50% - 85px)" });
+    // auth user
+    gP.identify({
+      userId: '12345678',
+      firstName: 'John',
+      lastName: 'Doe'
+    })
 
-      // gP.setPosition({ bottom: 50, right: 500 });
-    }, 1000);
-  }, []);
+    // show and position widget
+    gP.show()
+    gP.setButtonStyles({ bottom: '50px', right: 'calc(50% - 85px)' })
+
+    // gP.setPosition({ bottom: 50, right: 500 });
+    setLaunched(true)
+  }
+
+  const sendEvent = async () => {
+    try {
+      const res = await axios.post(
+        `https://dev.gopeer.org/users/event?userId=12345678&type=entered_assessment&token=${token}`
+      )
+      console.log(res)
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className='App'>
+      <header className='App-header'>
         <div
           onClick={() => {
-            const nextTheme = theme === "dark" ? "light" : "dark";
+            const nextTheme = theme === 'dark' ? 'light' : 'dark'
 
-            window.gP.setTheme(nextTheme);
-            theme = nextTheme;
+            window.gP.setTheme(nextTheme)
+            theme = nextTheme
           }}
         >
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src={logo} className='App-logo' alt='logo' />
         </div>
       </header>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 15,
+          alignItems: 'center'
+        }}
+      >
+        Platform
+        <input
+          placeholder='Set platform'
+          value={org}
+          onChange={(e) => {
+            setOrg(e.target.value)
+            localStorage.setItem('gp-pmid', e.target.value)
+          }}
+        />
+        Key
+        <input
+          placeholder='Set key'
+          value={key}
+          onChange={(e) => {
+            setKey(e.target.value)
+            localStorage.setItem('gp-key', e.target.value)
+          }}
+        />
+        <button onClick={loadToken}>Load Token</button>
+        {token && <button onClick={showButton}>Show Button</button>}
+        {launched && <button onClick={sendEvent}>Send Event</button>}
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
